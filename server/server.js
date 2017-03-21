76150f9a6ede1389186977d8600e9a352d9a5fe6
Andrea Takamiya <takamiya.andrea@gmail.com>
2017-03-20 20:57:13 -1000

    Boilerplate setup of Sequelize adn migrations

--- server/server.js
+++ server/server.js
@@ -1,2 +1,9 @@
-node_modules
-.env
+const express = require('express')
+const PORT = process.env.PORT || 3000
+const bp = require('body-parser')
+
+app.use(bp.urlencoded({ extended: true }))
+
+app.listen(PORT, function () {
+  console.log(`Listening on ${PORT}`)
+})
