3d87b206ebb5424a8285f015fce277566882b6fb
Andrea Takamiya <takamiya.andrea@gmail.com>
2017-03-20 22:51:34 -1000

    Successfully receiving api data, but using fetch. Need to do a standard xmlhttprequest instead

--- client/src/containers/KanbanContainer.js
+++ client/src/containers/KanbanContainer.js
@@ -1,56 +1,30 @@
 import React, {Component} from 'react';
-import Kanban from '../components/Kanban.js'
-import '../index.css'
+// import Kanban from '../components/Kanban.js';
+import '../index.css';
 
 class KanbanContainer extends Component {
   constructor(props){
-    super(props)
-
-    // this.state = []
-
-      this.state = {
-      bookList : [
-        {
-          _id: 1,
-          title: 'Ready Player One',
-          author: 'Ernest Cline'
-        },
-        {
-          _id: 2,
-          title: 'Enders Game',
-          author: 'Orson Scott Card'
-        },
-        {
-          _id: 3,
-          title: 'Bladerunner',
-          author: 'Harrison Ford'
-        }
-      ]
-    }
+    super(props);
+    this.state = {
+      cards: []
+    };
   }
 
-
-
-  // getCards(){
-  //   let req = new XMLHttpRequest()
-  //   req.open('GET', 'api/kanban')
-  // }
+  componentDidMount(){
+    fetch(`/api`)
+      .then(result => {
+        // result.json()
+        console.log(result);
+      })
+      // .then(items => this.setState({cards}))
+  }
 
   render() {
     return (
     <div>
       <h1>Test</h1>
 
-      <ul>
 
-      {this.state.bookList.map((book) =>
-      <Kanban
-        key={book._id}
-        title={book.title}
-        author={book.author}
-      />
-      )}
-    </ul>
   </div>
   )
   }
