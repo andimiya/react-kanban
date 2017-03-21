83b1f13cdadfe6ac912dcc73657b4e48495d8e22
Andrea Takamiya <takamiya.andrea@gmail.com>
2017-03-20 22:26:29 -1000

    Displaying dummy data on the page, ready to import from API now

--- client/src/containers/KanbanContainer.js
+++ client/src/containers/KanbanContainer.js
@@ -1,13 +1,35 @@
 import React, {Component} from 'react';
-// import Kanban from '../components/Kanban.js'
+import Kanban from '../components/Kanban.js'
 import '../index.css'
 
 class KanbanContainer extends Component {
   constructor(props){
     super(props)
 
-    this.state = []
+    // this.state = []
+
+      this.state = {
+      bookList : [
+        {
+          _id: 1,
+          title: 'Ready Player One',
+          author: 'Ernest Cline'
+        },
+        {
+          _id: 2,
+          title: 'Enders Game',
+          author: 'Orson Scott Card'
+        },
+        {
+          _id: 3,
+          title: 'Bladerunner',
+          author: 'Harrison Ford'
+        }
+      ]
+    }
   }
+
+
 
   // getCards(){
   //   let req = new XMLHttpRequest()
@@ -16,13 +38,21 @@ class KanbanContainer extends Component {
 
   render() {
     return (
-    <h1>Test</h1>
-    // <BookList
-    //   key={book._id}
-    //   title={book.title}
-    //   author={book.author}
-    // />
-    )
+    <div>
+      <h1>Test</h1>
+
+      <ul>
+
+      {this.state.bookList.map((book) =>
+      <Kanban
+        key={book._id}
+        title={book.title}
+        author={book.author}
+      />
+      )}
+    </ul>
+  </div>
+  )
   }
 }
 
