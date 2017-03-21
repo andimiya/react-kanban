f24972b69b4d602fae5f5f0478c7f8771956020d
Andrea Takamiya <takamiya.andrea@gmail.com>
2017-03-20 21:23:21 -1000

    Created post route to add a new card

--- server/server.js
+++ server/server.js
@@ -2,8 +2,24 @@ const express = require('express')
 const PORT = process.env.PORT || 3000
 const app = express()
 const bp = require('body-parser')
+const db = require('./models')
+const { Card } = db
 
 app.use(bp.urlencoded({ extended: true }))
+
+app.post('/new', (req, res) => {
+  Card.create({
+    title: req.body.title,
+    priority: req.body.priority,
+    status: req.body.status
+  })
+    .then(card => {
+      res.send('success')
+    })
+    .catch(() => {
+      res.send('error')
+    })
+})
 
 app.listen(PORT, function () {
   console.log(`Listening on ${PORT}`)
