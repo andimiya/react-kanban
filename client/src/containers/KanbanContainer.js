d44fa2adc02c02a3f4f4858f2ac2c4eeea769430
Andrea Takamiya <takamiya.andrea@gmail.com>
2017-03-22 18:22:22 -1000

    Changed back to my original ComponentDidMount withXHR Request. Also fixed the map function

--- client/src/containers/KanbanContainer.js
+++ client/src/containers/KanbanContainer.js
@@ -8,67 +8,43 @@ class KanbanContainer extends Component {
     this.state = {
       cards: []
     };
-    // this.reqListener = this.reqListener.bind(this)
-  // }
+  }
 
-      // ['cards'].map((cards) =>{
-      //   return (() =>{
-    let oReq = new XMLHttpRequest();
-    const reqListener = () =>{
-      console.log('oreq: ', oReq.response);
-      this.setState(
-        {[`cards`]: JSON.parse(oReq.response)}
-      );
-    };
-    oReq.addEventListener("load", reqListener);
-    oReq.open("GET", `http://localhost:8080/api`);
-    oReq.send();
-        // })
-        // ();
-      // });
-    }
-
-  //
-  // componentDidMount(){
-  //
-  //   var that = this;
-  //
-  //   function reqListener () {
-  //     var data = JSON.parse(this.responseText);
-  //     console.log(data, 'data');  //Object with an array
-  //     console.log(this, 'this render'); //XMLHttpRequest
-  //     console.log(that, 'that render');  //no data
-  //     console.log(that.responseText, 'that responseText'); //undefined
-  //     console.log(this.responseText, 'this responseText');  //data!!
-  //     console.log(this.setState({cards: data}), 'setstate');
-  //     this.setState({
-  //       cards: data
-  //     }.bind(this))
-  //   }
-
-    // var oReq = new XMLHttpRequest();
-    // oReq.addEventListener('load', reqListener);
-    // oReq.open('GET', 'http://localhost:8080/api');
-    // oReq.send();
-  // }
+  componentDidMount(){
+    let that = this;
+    function reqListener() {
+    console.log(this.responseText, 'responseText');
+    that.setState({
+      cards: JSON.parse(this.responseText)
+    })
+   }
+   var oReq = new XMLHttpRequest();
+   oReq.addEventListener('load', reqListener);
+   oReq.open('GET', 'http://localhost:8080/api');
+   oReq.send();
+  }
 
   render() {
-
-    var array = Object.keys(this.state.cards).map(key => this.state.cards[key]);
-    console.log(array, 'array');
-    console.log(this.state.cards, 'state')
-    console.log(this, 'this render')
-    this.state.cards = [1,2,3,4]
     return (
-      <KanbanColumn
-        cards={array
-          .filter((card) => {
-            return (
-              card.status === 'done'
-            )
-          })
-        }
-      />
+
+    <div>
+    {
+    this.state.cards.map(card => {
+      console.log(this.state.cards, 'state')
+      return (
+        <KanbanColumn
+          key={card.id}
+          status={card.status}
+        />
+      )
+    })
+    }
+    </div>
+    // console.log(array, 'array');
+    // console.log(this.state.cards, 'state')
+    // console.log(this, 'this render')
+    // return (
+    //   console.log(card, 'card');
     )
   }
 }
