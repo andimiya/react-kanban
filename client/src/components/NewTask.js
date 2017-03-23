daf7634d7b15753a7a94c3bbdfab0c65ea0e2708
Andrea Takamiya <takamiya.andrea@gmail.com>
2017-03-22 21:20:49 -1000

    Saving progress

--- client/src/components/NewTask.js
+++ client/src/components/NewTask.js
@@ -7,28 +7,42 @@ class NewTask extends Component {
     this.state = {
       cards: []
     }
-    this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({
-      cards: event.target.value
-    })
+    this.handleChangeTitle = this.handleChangeTitle.bind(this);
+    this.handleChangeStatus = this.handleChangeStatus.bind(this);
+    this.handleChangeStatus = this.handleChangeStatus.bind(this);
   }
 
   handleSubmit(event) {
-    alert(`Submitted`);
     event.preventDefault();
-    this.addTask({
+    console.log('handle submit triggered')
+    this.setState({
       title: this.state.title,
       priority: this.state.priority,
       status: this.state.status
     })
   }
 
+  handleChangeTitle(event){
+    this.setState({
+      title : event.target.value
+    })
+  }
+
+  handleChangePriority(event){
+    this.setState({
+      priority : event.target.value
+    })
+  }
+
+  handleChangeStatus(event){
+    this.setState({
+      status : event.target.value
+    })
+  }
+
   addTask(card) {
-    var formData = {
+    var Data = {
       title: this.state.title,
       priority: this.state.priority,
       status: this.state.status
@@ -36,16 +50,17 @@ class NewTask extends Component {
 
     var oReq = new XMLHttpRequest();
     oReq.open('POST', 'http://localhost:8080/new');
-    oReq.send(formData);
+    console.log(Data, 'data');
+    oReq.send(this.addTask);
   }
 
   render() {
     return (
       <form action="http://localhost:8080/new" method="post" onSubmit={this.handleSubmit}>
-          <input type="text" placeholder="title" value={this.state.title}  />
-          <input type="text" placeholder="priority" value={this.state.priority}  />
-          <input type="text" placeholder="status" value={this.state.status}  />
-        <input type="submit" value="Add Task" />
+          <input type="text" placeholder="title" value={this.state.title} onChange={this.handleChangeTitle}  />
+          <input type="text" placeholder="priority" value={this.state.priority} onChange={this.state.priority} />
+          <input type="text" placeholder="status" value={this.state.status} onChange={this.state.status} />
+        <input type="submit" value="Add Task" onSubmit={this.handleSubmit} />
       </form>
     )
   }
