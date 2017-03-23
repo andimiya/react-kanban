82e2664a7333618e5b7b3b0a3cdbdcd63a488c9d
Andrea Takamiya <takamiya.andrea@gmail.com>
2017-03-22 19:36:54 -1000

    Set the classname of each card to the card's status

--- client/src/components/KanbanCard.js
+++ client/src/components/KanbanCard.js
@@ -2,7 +2,7 @@ import React from 'react'
 import '../index.css'
 
 const KanbanCard = (props) => (
-  <div className="kanban-board">
+  <div className={props.status}>
     <h2>{props.title}</h2>
     <p>{ props.priority}</p>
     <p>{ props.status}</p>
