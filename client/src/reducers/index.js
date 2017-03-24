a3af1247c07f278df3d74dd04ad8f1a96641a836
Andrea Takamiya <takamiya.andrea@gmail.com>
2017-03-23 20:53:15 -1000

    Added reducer for cards

---
+++ client/src/reducers/index.js
@@ -0,0 +1,25 @@
+import { ADD_TASK } from '../actions';
+
+const initialState = {
+  cards: []
+};
+
+function cards(state = initialState, action) {
+  switch(action.type) {
+    case ADD_TASK:
+      return Object.assign({}, state, {
+        cards: [
+          ...state.cards,
+          {
+            title: action.title,
+            priority: action.priority,
+            status: action.status
+          }
+        ]
+      })
+    default:
+      return state;
+  }
+}
+
+export default cards;
