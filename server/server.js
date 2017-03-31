5bd9be5f56b7488fbc05622e6a063320a6990790
Andrea Takamiya <takamiya.andrea@gmail.com>
2017-03-30 15:07:45 -1000

    Config_example created

--- server/server.js
+++ server/server.js
@@ -8,12 +8,19 @@ const { Card } = db
 app.use(bp.urlencoded({ extended: true }))
 app.use(bp.json())
 
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-  next()
-})
+var allowCrossDomain = function(req, res, next) {
+    if ('OPTIONS' == req.method) {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
+app.use(allowCrossDomain);
 
 app.get('/api', (req, res) => {
   Card.findAll()
