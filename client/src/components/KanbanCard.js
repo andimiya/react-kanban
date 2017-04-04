4178044d61d4c34a410d92a035e121b13d8f0b5b
Andrea <takamiya.andrea@gmail.com>
2017-04-03 22:18:53 -1000

    Saving progress, almost completed refactor

--- client/src/components/KanbanCard.js
+++ client/src/components/KanbanCard.js
@@ -1,80 +1,27 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux';
-import { changeStatus } from '../actions';
+import React from 'react'
 import '../index.css'
 
-class KanbanCard extends Component {
+function KanbanCard(props){
+  return(
+    <div>
+    <div>
+      Title: {props.title}<br />
+      Priority: {props.priority}<br />
+      Status: {props.status}<br />
+    </div>
+    <div>
 
-  constructor(props){
-    super(props);
-
-    this.moveEvent = this.moveEvent.bind(this);
-    this.changeStatus = this.changeStatus.bind(this);
-
-  }
-
-  moveEvent(event){
-console.log(this.props.status, 'prop');
-    event.preventDefault();
-    // this.changeStatus(this.props)
-    // .then((card) => {
-    //   this.props.changeStatus(card.id, "Done")
-      console.log(event.target, 'move event');
-    // })
-  }
-
-  changeStatus(card){
-    console.log(event.target.value, 'change status event');
-    return new Promise(function(resolve, reject){
-      function reqListener(){
-        resolve(card)
+      {(props.status === 'To-Do' || props.status === 'In-Progress') &&
+        <button onClick={props.moveRight} value={props.status}> Right Arrow </button>
       }
 
-      let http = new XMLHttpRequest();
-      http.open("PUT", `http://localhost:8080/update/${card.id}`);
-      http.addEventListener("load", reqListener);
-      http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-      http.send(`status=${event.target.value}`);
-    })
-  }
+      {(props.status === 'In-Progress' || props.status === 'Done') &&
+        <button onClick={props.moveLeft} value={props.status}> Left Arrow </button>
+      }
+    </div>
 
-  render(){
-    return(
-      <div>
-      <div>
-        Title: {this.props.title}<br />
-        Priority: {this.props.priority}<br />
-        Status: {this.props.status}<br />
-      </div>
-      <div>
-      <button onClick={this.moveEvent} value="To-Do">To-Do</button>
-      </div>
-      <div>
-      <button onClick={this.moveEvent} value={"In-Progress"}>In-Progress</button>
-      </div>
-      <div>
-      <button onClick={this.moveEvent} value={"Done"}>Done</button>
-      </div>
-      </div>
-    )
-  }
+    </div>
+  )
 }
 
-const mapStateToProps = (state) => {
-  return {
-    cards: state.cards
-  }
-}
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    onChangeStatus: (id, status) => {
-      dispatch(changeStatus(id, status));
-    }
-  }
-};
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(KanbanCard);
+export default KanbanCard;
--- client/src/components/KanbanColumn.js
+++ client/src/components/KanbanColumn.js
@@ -4,6 +4,92 @@ import '../index.css'
 
 class Column extends Component {
 
+  constructor(props){
+    super(props);
+
+    this.moveToDo = this.moveToDo.bind(this);
+    this.moveToInProgress = this.moveToInProgress.bind(this);
+    this.moveToDone = this.moveToDone.bind(this);
+    this.moveRight = this.moveRight.bind(this);
+    this.moveLeft = this.moveLeft.bind(this);
+    this.changeStatus = this.changeStatus.bind(this);
+  }
+
+  moveRight(event){
+    console.log(this.props.cards);
+    if (event.target.value === 'To-Do'){
+      this.moveToInProgress()
+    }
+    else if (event.target.value === 'In-Progress'){
+      this.moveToDone(this.props.cards)
+    }
+  }
+
+  moveLeft(event){
+    if (event.target.value === 'In-Progress'){
+      this.moveToDo()
+    }
+    else if (event.target.value === 'Done'){
+      this.moveToInProgress(this.props.cards)
+    }
+  }
+
+  changeStatus(card){
+    return new Promise(function(resolve, reject){
+      function reqListener(){
+        resolve(card)
+      }
+
+      let http = new XMLHttpRequest();
+      http.open("PUT", `http://localhost:8080/update/${card.id}`);
+      http.addEventListener("load", reqListener);
+      http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+      http.send(`status=${event.target.value}`);
+    })
+  }
+
+  moveToDo(card){
+    return new Promise(function(resolve, reject){
+      function reqListener(){
+        resolve(card)
+      }
+
+      let http = new XMLHttpRequest();
+      http.open("PUT", `http://localhost:8080/update/${card.id}`);
+      http.addEventListener("load", reqListener);
+      http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+      http.send(`status=To-Do`);
+    })
+  }
+
+  moveToInProgress(card){
+    return new Promise(function(resolve, reject){
+      function reqListener(){
+        resolve(card)
+      }
+
+      let http = new XMLHttpRequest();
+      http.open("PUT", `http://localhost:8080/update/${card.id}`);
+      http.addEventListener("load", reqListener);
+      http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+      http.send(`status=In-Progress`);
+    })
+  }
+
+  moveToDone(card){
+    return new Promise(function(resolve, reject){
+      function reqListener(){
+        resolve(card)
+      }
+
+      let http = new XMLHttpRequest();
+      http.open("PUT", `http://localhost:8080/update/${card.id}`);
+      http.addEventListener("load", reqListener);
+      http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+      http.send(`status=Done`);
+    })
+  }
+
   render() {
     return (
       <div className="column">
@@ -18,6 +104,7 @@ class Column extends Component {
               title={card.title}
               priority={card.priority}
               status={card.status}
+              moveRight={this.moveRight}
             />
           )
         })
@@ -33,6 +120,8 @@ class Column extends Component {
               title={card.title}
               priority={card.priority}
               status={card.status}
+              moveRight={this.moveRight}
+              moveLeft={this.moveLeft}
             />
           )
         })
@@ -48,6 +137,7 @@ class Column extends Component {
               title={card.title}
               priority={card.priority}
               status={card.status}
+              moveLeft={this.moveLeft}
             />
           )
         })
