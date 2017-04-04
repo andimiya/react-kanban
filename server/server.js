89179472b81e9e7f0da8ca69cff62c8f21466f20
Andrea <takamiya.andrea@gmail.com>
2017-04-03 19:56:54 -1000

    Updated CORS Methods

--- server/server.js
+++ server/server.js
@@ -7,20 +7,12 @@ const { Card } = db
 
 app.use(bp.urlencoded({ extended: true }))
 app.use(bp.json())
-
-var allowCrossDomain = function(req, res, next) {
-    if ('OPTIONS' == req.method) {
-      res.header('Access-Control-Allow-Origin', '*');
-      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
-      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-      res.send(200);
-    }
-    else {
-      next();
-    }
-};
-
-app.use(allowCrossDomain);
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
 
 app.get('/api', (req, res) => {
   Card.findAll()
