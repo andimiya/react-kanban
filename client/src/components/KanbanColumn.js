f230b3e354dc21d17c70b978e1abb604e9856912
Andrea <takamiya.andrea@gmail.com>
2017-04-04 10:31:44 -1000

    Saving progress

--- client/src/components/KanbanColumn.js
+++ client/src/components/KanbanColumn.js
@@ -9,9 +9,6 @@ class Column extends Component {
   constructor(props){
     super(props);
 
-    // this.moveToDo = this.moveToDo.bind(this);
-    // this.moveToInProgress = this.moveToInProgress.bind(this);
-    // this.moveToDone = this.moveToDone.bind(this);
     this.moveStatus = this.moveStatus.bind(this);
     this.moveRight = this.moveRight.bind(this);
     this.moveLeft = this.moveLeft.bind(this);
@@ -47,44 +44,15 @@ class Column extends Component {
       }
       console.log(id, 'card id');
       console.log(status, 'card status');
-      // this.props.onChangeStatus('24', 'In-Progress')
+      console.log(this.onChangeStatus, 'props new');
       let http = new XMLHttpRequest();
+
       http.open("PUT", `http://localhost:8080/update/${id}`);
       http.addEventListener("load", reqListener);
       http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
       http.send(`status=${status}`)
-
     })
   }
-  //
-  // moveToInProgress(id){
-  //   console.log('test');
-  //   return new Promise(function(resolve, reject){
-  //     function reqListener(){
-  //       resolve()
-  //     }
-  //
-  //     let http = new XMLHttpRequest();
-  //     http.open("PUT", `http://localhost:8080/update/${id}`);
-  //     http.addEventListener("load", reqListener);
-  //     http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-  //     http.send(`status=In-Progress`);
-  //   })
-  // }
-  //
-  // moveToDone(id){
-  //   return new Promise(function(resolve, reject){
-  //     function reqListener(){
-  //       resolve()
-  //     }
-  //
-  //     let http = new XMLHttpRequest();
-  //     http.open("PUT", `http://localhost:8080/update/${id}`);
-  //     http.addEventListener("load", reqListener);
-  //     http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-  //     http.send(`status=Done`);
-  //   })
-  // }
 
   render() {
     return (
@@ -94,7 +62,6 @@ class Column extends Component {
       { this.props.cards
         .filter (card => card.status === "To-Do")
         .map (card => {
-          console.log(this.props, 'props');
           return (
             <KanbanCard
               key={card.id}
@@ -153,7 +120,6 @@ class Column extends Component {
     )
   }
 };
-
 
 const mapStateToProps = (state) => {
   return {
